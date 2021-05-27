57ee0f53f9c785cd818d9021be90f187fd3c2360
Charles McCullough <charles@charlesmccullough.com>
2021-05-26 20:50:30 -0500

    End of Section 39

--- Application/Activities/Delete.cs
+++ Application/Activities/Delete.cs
@@ -1,36 +1,31 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
-using AutoMapper;
-using Domain;
 using MediatR;
 using Persistence;
 
 namespace Application.Activities
 {
-    public class Edit
+    public class Delete
     {
         public class Command : IRequest
         {
-            public Activity Activity { get; set; }
+            public Guid Id { get; set; }
         }
 
         public class Handler : IRequestHandler<Command>
         {
-            private  IMapper _mapper { get; }
             private readonly DataContext _context;
-        public IMapper Mapper { get; }
-
-            public Handler(DataContext context, IMapper mapper)
+            public Handler(DataContext context)
             {
-                _mapper = mapper;
                 _context = context;
             }
-           
+
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
-                var activity = await _context.Activities.FindAsync(request.Activity.Id);
+                var activity = await _context.Activities.FindAsync(request.Id);
 
-                _mapper.Map(request.Activity, activity);
+                _context.Remove(activity);
 
                 await _context.SaveChangesAsync();
 
